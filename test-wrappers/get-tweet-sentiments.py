5d602ab2ac7d94e0a2a6a3e5976d0c013fd12835
David Turanski <dturanski@pivotal.io>
2017-05-10 18:52:24 -0400

    Add get-tweet-sentiments.py

---
+++ test-wrappers/get-tweet-sentiments.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+Parse a list of tweets, extract the text
+'''
+import json
+
+def input(payload):
+	tweets=json.loads(payload)
+	text = []
+	
+	for tweet in tweets:
+		if tweet['lang'] == 'en':
+			val = tweet['text'].encode('ascii','ignore')
+			text.append(val)
+
+	return json.JSONEncoder().encode({"data": text })
+
+def output(payload):
+	return payload	
+
+if __name__ == '__main__':
+	with open('./list-of-tweets.txt', 'r') as tweets:
+		payload=tweets.read().replace('\n', '')
+	channel='input'	
+	
+result=locals()[channel](payload)
+print(result)
