8c60f26f06fa82c98d1fa13e8f955c0ce9e98e7a
David Turanski <dturanski@pivotal.io>
2017-05-17 00:24:29 -0400

    Fix float conversion bug

--- test-wrappers/map-tweet-sentiments.py
+++ test-wrappers/map-tweet-sentiments.py
@@ -3,26 +3,25 @@ import json
 
 def label_sentiment_score(payload):
     sentiments = dict(
-        (name.title(), eval(name)) for name in ['ecstatic', 'happy', 'warm', 'meh', 'cool', 'gloomy', 'doomed'])
+        (name.title(), float(eval(name))) for name in ['ecstatic', 'happy', 'warm', 'meh', 'cool', 'gloomy', 'doomed'])
     doc = json.loads(payload)
     score = float(doc['polarity'])
 
     sentiment='Gloomy'
-    for (k, v) in sorted(sentiments.items(), key=lambda item: (item[1], item[0])):
+    for (k, v) in sorted(sentiments.items(), key=lambda x: x[1]):
         if score > v:
             sentiment = k
-    print("polarity:%f sentiment:%s" % (score, sentiment))        
     return json.dumps({'sentiment': sentiment})
 
 
 if __name__ == "__main__":
-    ecstatic = .90
-    happy = .75
-    warm = .65
-    meh = .50
-    cool = .35
-    gloomy = 0.25
-    doomed = 0.00
+    ecstatic = '.90'
+    happy = '.75'
+    warm = '.65'
+    meh = '.50'
+    cool = '.35'
+    gloomy = '0.25'
+    doomed = '0.00'
 
     import sys
 
