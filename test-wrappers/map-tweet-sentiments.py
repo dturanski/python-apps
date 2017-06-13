f2bf1c6f839e1aa63fae8826d051a613ec2ce69b
David Turanski <dturanski@pivotal.io>
2017-06-13 06:12:06 -0400

    Remove test code

--- test-wrappers/map-tweet-sentiments.py
+++ test-wrappers/map-tweet-sentiments.py
@@ -1,33 +1,7 @@
 import json
 #
-# For testing, the module is not __main__ at runtime
-#
-if __name__ == "__main__":
-
-    def new_item(text, polarity):
-        return json.dumps({'text':text,'polarity':polarity})
-
-    ecstatic = '.90'
-    happy = '.75'
-    warm = '.65'
-    meh = '.50'
-    cool = '.35'
-    gloomy = '0.25'
-    doomed = '0.00'
-
-
-    items = []
-    items.append(new_item('some text',0.4102732628007269641027326280072696))
-    items.append(new_item('some text', 0.2102732628007269641027326280072696))
-    items.append(new_item('some text', 0))
-    items.append(new_item('some text', 0.6102732628007269641027326280072696))
-    items.append(new_item('some text', 0.9102732628007269641027326280072696))
-    items.append(new_item('some text', 0.75))
-
-#
 # Processor script
 #
-
 
 def label_sentiment_score(payload):
     sentiments = dict(
@@ -47,8 +21,4 @@ def label_sentiment_score(payload):
             sentiment = k
     return json.dumps({'sentiment': sentiment})
 
-if __name__ == "__main__":
-    for item in items:
-        print(label_sentiment_score(item))
-else:
-    result = label_sentiment_score(payload)
+result = label_sentiment_score(payload)
